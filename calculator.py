ee7d0cc64db62d6fdc6f5642df74649380bcba29
ArdiK_ <karpson153@gmail.com>
2018-10-21 03:50:31 +0300

    2nd

--- calculator.py
+++ calculator.py
@@ -1,4 +1,25 @@
-operation = input("Insert operation: ")
-calculation = eval(operation)
-print()
-print(f" {operation} = {calculation}")
+operation = input("What you would like to calculate?: ")
+splitted = operation.split(" ")
+
+if len(splitted) != 3:
+	print("Error: You have to insert correct calculation. Example: 3 + 3")
+
+first, type, second = splitted
+
+first = int(first)
+second = int(second)
+
+if type == "+":
+	summation = first + second
+	print(f"Answer: {summation}")
+elif type == "-":
+	subtraction = first - second
+	print(f"Answer: {subtraction}")
+elif type == "*":
+	multiplication = first * second
+	print(f"Answer: {multiplication}")
+elif type == "/":
+	split = first / second
+	print(f"Answer: {split}")
+else:
+	print("Error: Invalid operation type. Please use (+; -; *; /;).")
